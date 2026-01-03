ca5d199d41daf1080fa355c6d39b6a9eadf807ac
shar-hylton <shar.hyl10@gmail.com>
2026-01-02 23:57:01 -0500

    server router form validaiton and authentication

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -8,6 +8,8 @@ const userSchema = mongoose.Schema(
       unique: true,
       trim: true,
       lowercase: true,
+      minlength: 3,
+      maxlength: 15,
     },
 
     email: {
@@ -21,9 +23,20 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
       select: false, // prevents hashes from leaking
+      minlength: 8,
+      maxlength: 32,
+      validate: {
+        validator: function (value) {
+          return /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/.test(
+            value
+          );
+        },
+        message:
+          "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+      },
     },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
